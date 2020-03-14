9f8be33b1a4c29704bc01974628b05647b4bf8e1
lewisd1996 <lewis.d1996@gmail.com>
2020-03-14 14:39:37 +0000

    Updates

---
+++ precache-manifest.9b64bc842f334877cac20f084f936619.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e754a730a3a4b34c5d386b10f269491",
+    "url": "/lewisdonley/index.html"
+  },
+  {
+    "revision": "f5c1f759169522f76eec",
+    "url": "/lewisdonley/static/css/2.3c814ef1.chunk.css"
+  },
+  {
+    "revision": "b7da7f9ada65fddfcaec",
+    "url": "/lewisdonley/static/css/main.7f354618.chunk.css"
+  },
+  {
+    "revision": "f5c1f759169522f76eec",
+    "url": "/lewisdonley/static/js/2.d0fbb385.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/lewisdonley/static/js/2.d0fbb385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7da7f9ada65fddfcaec",
+    "url": "/lewisdonley/static/js/main.55d39039.chunk.js"
+  },
+  {
+    "revision": "e7242cf52c8faf70b373",
+    "url": "/lewisdonley/static/js/runtime-main.939e2eeb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/lewisdonley/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/lewisdonley/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/lewisdonley/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/lewisdonley/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/lewisdonley/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
